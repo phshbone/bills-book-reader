6dadf2d30df2b7550af22bd07cb1b9bcdaf637a7
phshbone <phshbone@gmail.com>
2026-09-23 07:37:33 -0400

    Refresh PWA cache for appearance sheet refinement

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bills-book-reader-v8';
+const CACHE = 'bills-book-reader-v9';
 const SHELL = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/book.svg',
   './vendor/jszip.min.js', './vendor/epub.min.js'
