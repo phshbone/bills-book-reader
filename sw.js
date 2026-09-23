9e72b4548e6bb31d56492df4e6bc973df3f79cb1
phshbone <phshbone@gmail.com>
2026-09-23 08:28:18 -0400

    Refresh PWA cache for centered status bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bills-book-reader-v9';
+const CACHE = 'bills-book-reader-v10';
 const SHELL = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/book.svg',
   './vendor/jszip.min.js', './vendor/epub.min.js'
