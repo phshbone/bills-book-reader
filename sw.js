8eb2cbb3fd8b22cfb7bb3295ae6236464a2a65bb
phshbone <phshbone@gmail.com>
2026-09-24 17:36:23 -0400

    Refresh PWA cache for continuous chapter scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bills-book-reader-v14';
+const CACHE = 'bills-book-reader-v15';
 const SHELL = [
   './', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/book.svg',
   './vendor/jszip.min.js', './vendor/epub.min.js'
